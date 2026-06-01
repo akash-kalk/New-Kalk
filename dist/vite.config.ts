612d7e22378d9f9febc3b1f3d5568428fde2f298
akash-kalk <akash@kalksolutions.com>
2026-06-01 18:52:44 +0530

    Add files via upload

---
+++ dist/vite.config.ts
@@ -0,0 +1,10 @@
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  base: "/Kalk-Solutions/",
+  plugins: [TanStackRouterVite({ target: "react", autoCodeSplitting: true }), react(), tailwindcss(), tsconfigPaths()],
+});
